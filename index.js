827c9c9a712dfd6d31c0b3d6d60db5aab4ec70c5
kaungmyattun <eizokaungmyatsktun@gmail.com>
2022-09-24 22:16:06 +0630

    getting post url encoding value

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 let http = require('http');
 let urls = require('url');
+let qs = require('querystring')
 require('dotenv').config()
+
+let responder = (req, res, param) => {
+    res.writeHead(200, { "Content-Type": "text/html" })
+    res.end(param);
+}
 
 let routes = {
     "GET": {
-        "/": (req, res, param) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(`<h1>Get Method => / route with ${param.query.name}</h1>`)
-        },
-        "/home": (req, res, param) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(`<h1>Get Method => / route</h1> with ${param.query.name}`)
-        }
+        "/": (req, res) =>
+            responder(req, res, `<h1>Get Method => / route with ${params.query.name}</h1>`)
+        ,
+        "/home": (req, res) =>
+            responder(req, res, `<h1>Get Method => / route</h1> with ${params.query.name}`)
     },
     "POST": {
-        "/": (req, res, param) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(`<h1>POST Method => / route</h1> with ${param.query.name}`)
-        },
-        "/about": (req, res, param) => {
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(`<h1>POST Method => /about</h1> with ${param.query.name}`)
+        "/": (req, res) =>
+            responder(req, res, `<h1>POST Method => / route</h1> with ${params.query.name}`)
+        ,
+        "/about": (req, res, params) =>
+            responder(req, res, `<h1>POST Method => / about</h1> with ${params.query.name}`)
+        ,
+        "/api/login": (req, res, params) => {
+            let body = "";
+            req.on("data", data => {
+                body += data;
+            })
+            req.on("end", () => {
+                let query = qs.parse(body);
+
+                console.log(query.email);
+                res.end();
+            })
         }
     },
-    "NA": (req, res, param) => {
-        res.writeHead(404);
-        res.end("<h1>Not Found!</h1>")
-    }
+    "NA": (req, res) =>
+        responder(req, res, "<h1>Not Found!</h1>")
 }
 
 let start = (req, res) => {
     let reqMethod = req.method;
-    let param = urls.parse(req.url, true);
-    let resolveRoute = routes[req.method][param.pathname];
+    let params = urls.parse(req.url, true);
+    let resolveRoute = routes[req.method][params.pathname];
     if (resolveRoute != null && resolveRoute != undefined) {
-        resolveRoute(req, res, param);
+        resolveRoute(req, res, params);
     } else {
-        routes["NA"](req,res, param);
+        routes["NA"](req, res);
     }
 }
 
