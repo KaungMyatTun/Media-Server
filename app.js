67aad1bf68eb73c7949270b8426d14de877a57c5
kaungmyattun <eizokaungmyatsktun@gmail.com>
2022-10-09 22:24:16 +0630

    9/10/2022 Lession

--- app.js
+++ app.js
@@ -4,20 +4,50 @@ let express = require('express'),
     path = require('path'),
     server = require('http').createServer(app),
     hogan = require('hogan-express'),
-    {Server} = require('socket.io'), 
-    io = new Server(server);
+    { Server } = require('socket.io'),
+    io = new Server(server),
+    helper = require('./bmlibby');
+
 
 app.engine('html', hogan);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, "assets")))
 
-io.on('listening', function(){
+io.on('listening', function () {
     console.log(`listening on port ${io.server.PORT}`);
 })
 
-app.get('/login', (req,res)=>{
+app.get('/login', (req, res) => {
     res.render('index');
 })
+
+/**
+  #sending to all users
+  io.emit("key", "val");
+
+  #send message only to sender client
+  socket.emit("key", "val");
+
+  #send to all listener except sender
+  socket.broadcast.emit("chat-data", username + " : " + data);
+
+  #send message to certain user
+  io.to(socket.id).emit();
+
+  #send to specifi socket-id (specific user)
+  socket.broadcast.to(socketid).emit("chat-data", username + " : " + data);
+ */
+
+io.sockets.on('connection', (socket) => {
+    socket.on('login', data => {
+        socket.username = data;
+        io.to(socket.id).emit('login-success', true);
+    });
+    socket.on('msg', data => {
+        io.emit('income-msg', socket.username + ": " + data);
+    })
+})
+
 
 server.listen(process.env.PORT, () => {
     console.log(`Server is running at ${process.env.PORT}`);
--- bmlibby/index.js
+++ bmlibby/index.js
@@ -4,6 +4,7 @@ let encode = (plainPassword) => {
         bcrypt.genSalt(10, function (err, salt) {
             bcrypt.hash(plainPassword, salt, function (err, hash) {
                 if (err) reject(err);
+                console.log(hash);
                 resolve(hash);
             });
         });
@@ -14,6 +15,7 @@ let compare = (plainPassword, hasPassword) => {
     return new Promise((resolve, reject) => {
         bcrypt.compare(plainPassword, hasPassword, (error, bool) => {
             if (error) reject(err);
+            console.log(bool);
             resolve(bool);
         })
     })
